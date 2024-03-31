aecd4e21be6069fcb3f129cb3314243ae044e584
Glendon Chin <glendonchin@gmail.com>
2024-03-31 15:47:11 +0900

    add and style footer

---
+++ pnr/src/components/footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="footer-col">
+          <div className="footer-item">
+            <a href="#">a</a>
+          </div>
+          <div className="footer-item">
+            <a href="#">b</a>
+          </div>
+          <div className="footer-item">
+            <a href="#">c</a>
+          </div>
+          <div className="footer-item">
+            <a href="#">d</a>
+          </div>
+          <div className="footer-item">
+            <a href="#">e</a>
+          </div>
+        </div>
+        <div className="footer-col">
+          <div className="footer-item">
+            <a href="#">f</a>
+          </div>
+          <div className="footer-item">
+            <a href="#">g</a>
+          </div>
+          <div className="footer-item">
+            <a href="#">h</a>
+          </div>
+          <div className="footer-item">
+            <a href="#">i</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
