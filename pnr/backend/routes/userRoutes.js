81e27bc7d34b1e6bb6b027a8c2882fc712f72e73
Glendon Chin <glendonchin@gmail.com>
2024-04-01 19:44:57 +0900

    create user routes

--- pnr/backend/routes/userRoutes.js
+++ pnr/backend/routes/userRoutes.js
@@ -16,9 +16,24 @@ router.post('/signup', async (req, res) => {
     });
 
     await newUser.save();
-    res.status(201).send('User created successfully.');
+    res.status(201).json({ message: 'User created successfully.' });
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (user && await bcrypt.compare(password, user.password)) {
+      res.status(200).json({ message: 'Login successful' });
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 });
 
