fe4f01562d5750898a0c09bb7de0a0ef599c8fbb
Glendon Chin <glendonchin@gmail.com>
2024-04-06 15:59:39 +0900

    implement name, tag to writing convo route

--- pnr/backend/routes/writingConversationRoutes.js
+++ pnr/backend/routes/writingConversationRoutes.js
@@ -8,25 +8,37 @@ const { v4: uuidv4 } = require('uuid');
 
 router.post('/save', async (req, res) => {
   try {
-    const conversationId = uuidv4();
 
+    const { participants, messages, name, tag } = req.body;
+  
+    if (!name || !tag) {
+      return res.status(400).send({ message: "Missing conversation name or tag." });
+    }
+    const conversationId = uuidv4();
     const newConversation = new Conversation({
-      ...req.body,
+      participants: participants.map(id => new mongoose.Types.ObjectId(id)),
+      messages: messages.map(message => ({
+        text: message.text,
+        from: new mongoose.Types.ObjectId(message.from),
+        timestamp: message.timestamp || new Date(),
+      })),
+      name,
+      tag,
       conversationId,
     });
 
     await newConversation.save();
-    res.status(201).send({ message: 'Conversation saved successfully', conversationId }); // Return the ID
+    res.status(201).send({ message: 'Conversation saved successfully', conversationId });
   } catch (error) {
     console.error("Error saving conversation:", error);
-    res.status(400).send(error);
+    res.status(400).send({ message: "Error saving conversation", details: error.message });
   }
 });
 
 router.post('/userMessages/save', async (req, res) => {
   try {
-    const { userId, messages, conversationId } = req.body;
-    const newUserMessageDoc = new Conversation({
+    const { userId, conversationId, messages, name, tag } = req.body;
+    const newConversation = new Conversation({
       participants: [new mongoose.Types.ObjectId(userId)],
       messages: messages.map(text => ({
         text,
@@ -34,15 +46,19 @@ router.post('/userMessages/save', async (req, res) => {
         timestamp: new Date(),
       })),
       conversationId,
+      name,
+      tag,
     });
 
-    await newUserMessageDoc.save();
+    await newConversation.save();
+
     res.status(201).send({ message: 'User messages saved successfully' });
   } catch (error) {
     console.error("Error saving user messages:", error);
-    res.status(400).send(error);
+    res.status(400).send({ message: "Failed to save user messages", details: error.message });
   }
 });
+
 
 
 module.exports = router;
