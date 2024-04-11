f8c06d6081acb091e79608d9591208340f2a6296
Glendon Chin <glendonchin@gmail.com>
2024-04-11 16:30:33 +0900

    add contact form

--- pnr/src/components/footer/Footer.jsx
+++ pnr/src/components/footer/Footer.jsx
@@ -1,23 +1,31 @@
-import React from "react";
-
+import React, { useState } from 'react';
+import ContactOverlay from '../contactoverlay/ContactOverlay';
 import "./footer.css";
 
 const Footer = () => {
+  const [isOverlayOpen, setIsOverlayOpen] = useState(false);
+
   return (
-    <div className="footer">
-      <div className="container">
-        <div className="footer-col">
-          <div className="footer-item">
-            <a href="#">Contact</a>
+    <>
+      <ContactOverlay isOpen={isOverlayOpen} setIsOpen={setIsOverlayOpen} />
+      <div className="footer">
+        <div className="container">
+          <div className="footer-col">
+            <div className="footer-item">
+              <a href="#" onClick={(e) => {
+                e.preventDefault();
+                setIsOverlayOpen(true);
+              }}>Contact</a>
+            </div>
           </div>
-        </div>
-        <div className="footer-col">
+          <div className="footer-col">
           <div className="footer-item">
             <a href="#">Est. Apr 2024</a>
           </div>
         </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
