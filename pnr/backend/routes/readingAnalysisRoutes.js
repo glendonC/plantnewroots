d80ef0eb50bd0ca795de1a2dbf665a2af16c85d0
Glendon Chin <glendonchin@gmail.com>
2024-04-20 14:57:14 +0900

    routes for reading analysis

--- pnr/backend/routes/readingAnalysisRoutes.js
+++ pnr/backend/routes/readingAnalysisRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getReadingSessions, getReadingSessionDetails, getReadingSessionsByConversationId, getReadingAnalysisDetails } = require('../controllers/readingAnalysisController');
+const { getReadingSessions, getReadingSessionDetails, getReadingSessionsByConversationId, getReadingAnalysisDetails, saveReadingAnalysis, fetchReadingAnalysis } = require('../controllers/readingAnalysisController');
 
 router.get('/', getReadingSessions);
-
 router.get('/:id', getReadingSessionDetails);
-
 router.get('/by-conversation/:conversationId', getReadingSessionsByConversationId);
 router.get('/reading-analysis/:id', getReadingAnalysisDetails);
+router.post('/reading-analysis/save', saveReadingAnalysis);
+router.get('/reading-analysis/fetch/:conversationId', fetchReadingAnalysis);
 
 module.exports = router;
