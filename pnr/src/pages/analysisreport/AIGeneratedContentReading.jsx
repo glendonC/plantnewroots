335e977ea741ff2e3b15af1c94072f91d9cf735a
Glendon Chin <glendonchin@gmail.com>
2024-04-20 15:16:02 +0900

    update format

--- pnr/src/pages/analysisreport/AIGeneratedContentReading.jsx
+++ pnr/src/pages/analysisreport/AIGeneratedContentReading.jsx
@@ -2,16 +2,31 @@ import React from 'react';
 
 const AIGeneratedContentReading = ({ loading, generatedText }) => {
   if (loading) return <p>Loading...</p>;
+
   if (!generatedText) return <p>No recommendations generated yet.</p>;
+
   const sections = generatedText.split("**").filter(text => text.trim() !== "");
+
+  let lastTitle = null;
 
   return (
     <div>
-      {sections.map((section, index) => (
-        <div key={index}>
-          <p>{section}</p>
-        </div>
-      ))}
+      {sections.map((section, index) => {
+        let title;
+        if (section.endsWith(":")) {
+          if (lastTitle !== section) {
+            title = <h3>{section}</h3>;
+            lastTitle = section;
+          }
+        }
+
+        return (
+          <div key={index}>
+            {title}
+            <p>{title ? "" : section}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
