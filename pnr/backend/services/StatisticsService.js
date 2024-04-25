0d99b20abfcf8832653c4677fbb38ec11a323c48
Glendon Chin <glendonchin@gmail.com>
2024-04-25 15:33:34 +0900

    create service for backend stats controller

---
+++ pnr/backend/services/StatisticsService.js
@@ -0,0 +1,9 @@
+exports.fetchAllStatistics = async () => {
+    return {
+        grammarAccuracy: 95,
+        vocabularyLevel: 90,
+        comprehensionScore: 88,
+        engagementLevel: 80,
+        trends: [{ month: 'January', score: 75 }, { month: 'February', score: 85 }]
+    };
+};
