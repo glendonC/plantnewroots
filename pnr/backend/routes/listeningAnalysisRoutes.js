b2d28bfedce2a2e4de2376deb85460e86c069efa
Glendon Chin <glendonchin@gmail.com>
2024-04-26 14:52:46 +0900

    update listening analysis routes

--- pnr/backend/routes/listeningAnalysisRoutes.js
+++ pnr/backend/routes/listeningAnalysisRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const authenticate = require('../middleware/authenticate');
 const { 
     getListeningAnalysisDetails, 
     saveListeningAnalysis, 
     fetchListeningAnalysis 
 } = require('../controllers/listeningAnalysisController');
 
-router.get('/listening-analysis/:id', getListeningAnalysisDetails);
-router.post('/listening-analysis/save', saveListeningAnalysis);
-router.get('/listening-analysis/fetch/:conversationId', fetchListeningAnalysis);
+router.get('/listening-analysis/:id', authenticate, getListeningAnalysisDetails);
+router.post('/listening-analysis/save', authenticate, saveListeningAnalysis);
+router.get('/listening-analysis/fetch/:conversationId', authenticate, fetchListeningAnalysis);
 
 module.exports = router;
