e38de3a8aa2ed1433aa1bb7a3897315265a9f2ee
Glendon Chin <glendonchin@gmail.com>
2024-04-26 14:20:43 +0900

    add auth to routes

--- pnr/backend/routes/readingAnalysisRoutes.js
+++ pnr/backend/routes/readingAnalysisRoutes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { getReadingSessions, getReadingSessionDetails, getReadingSessionsByConversationId, getReadingAnalysisDetails, saveReadingAnalysis, fetchReadingAnalysis } = require('../controllers/readingAnalysisController');
+const authenticate = require('../middleware/authenticate');
+const {
+    getReadingSessions,
+    getReadingSessionDetails,
+    getReadingSessionsByConversationId,
+    getReadingAnalysisDetails,
+    saveReadingAnalysis,
+    fetchReadingAnalysis
+} = require('../controllers/readingAnalysisController');
 
-router.get('/', getReadingSessions);
-router.get('/:id', getReadingSessionDetails);
-router.get('/by-conversation/:conversationId', getReadingSessionsByConversationId);
-router.get('/reading-analysis/:id', getReadingAnalysisDetails);
-router.post('/reading-analysis/save', saveReadingAnalysis);
-router.get('/reading-analysis/fetch/:conversationId', fetchReadingAnalysis);
+router.get('/', authenticate, getReadingSessions);
+router.get('/:id', authenticate, getReadingSessionDetails);
+router.get('/by-conversation/:conversationId', authenticate, getReadingSessionsByConversationId);
+router.get('/reading-analysis/:id', authenticate, getReadingAnalysisDetails);
+router.post('/reading-analysis/save', authenticate, saveReadingAnalysis);
+router.get('/reading-analysis/fetch/:conversationId', authenticate, fetchReadingAnalysis);
 
 module.exports = router;
