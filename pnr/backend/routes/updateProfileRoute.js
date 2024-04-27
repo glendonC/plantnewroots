23689b8fc088eec454a5e2985cb01b7844730036
Glendon Chin <glendonchin@gmail.com>
2024-04-27 15:04:42 +0900

    add forgot password route

--- pnr/backend/routes/updateProfileRoute.js
+++ pnr/backend/routes/updateProfileRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const authenticate = require('../middleware/authenticate');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
-
+const authenticate = require('../middleware/authenticate');
 const router = express.Router();
 
 router.post('/update-profile', authenticate, async (req, res) => {
@@ -22,7 +22,8 @@ router.post('/update-profile', authenticate, async (req, res) => {
             existingUser.email = email;
         }
         if (password) {
-            existingUser.password = password;
+            const hashedPassword = await bcrypt.hash(password, 12);
+            existingUser.password = hashedPassword;
         }
 
         existingUser = await existingUser.save();
@@ -31,6 +32,31 @@ router.post('/update-profile', authenticate, async (req, res) => {
         return res.json({ message: 'Profile updated successfully', user: existingUser });
     } catch (error) {
         console.error('Error updating profile:', error);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
+
+router.post('/forgot-password', async (req, res) => {
+    try {
+        const { email, newPassword } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            console.log('User not found');
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword, 12);
+
+       // console.log('New password hashed:', hashedPassword);
+        user.password = hashedPassword;
+        await user.save();
+
+        //console.log('Password reset successfully');
+        return res.status(200).json({ message: 'Password reset successfully' });
+    } catch (error) {
+        console.error('Error resetting password:', error);
         return res.status(500).json({ error: 'Internal server error' });
     }
 });
