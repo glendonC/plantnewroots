bb06db56bf0cb0be16343c333d5b0f37c5841077
Glendon Chin <glendonchin@gmail.com>
2024-04-27 12:50:20 +0900

    stacked bar chart component

---
+++ pnr/src/components/charts/StackedBarChart.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const StackedBarChart = ({ data }) => {
+  const options = {
+    plugins: {
+      title: {
+        display: true,
+        text: 'Analysis Count by Type'
+      }
+    },
+    scales: {
+      x: {
+        stacked: true,
+      },
+      y: {
+        stacked: true
+      }
+    },
+    responsive: true,
+    maintainAspectRatio: false
+  };
+
+  return (
+    <div style={{ height: '400px', width: '600px' }}>
+      <Bar data={data} options={options} />
+    </div>
+  );
+}
+
+
+export default StackedBarChart;
