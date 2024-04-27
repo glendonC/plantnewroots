54eaf4be57432ca6121934c223a456eebc1e0229
Glendon Chin <glendonchin@gmail.com>
2024-04-27 12:49:43 +0900

    add stat controller

---
+++ pnr/backend/controllers/statsController.js
@@ -0,0 +1,57 @@
+const ListeningAnalysis = require('../models/ListeningAnalysis');
+const ReadingAnalysis = require('../models/ReadingAnalysis');
+const SpeakingAnalysis = require('../models/SpeakingAnalysis');
+const WritingConversationAnalysis = require('../models/WritingConversationAnalysis');
+
+function getDateRange(filter) {
+    switch (filter) {
+      case 'today':
+        return { start: moment().startOf('day').toDate(), end: moment().endOf('day').toDate() };
+      case 'week':
+        return { start: moment().startOf('week').toDate(), end: moment().endOf('week').toDate() };
+      case 'month':
+        return { start: moment().startOf('month').toDate(), end: moment().endOf('month').toDate() };
+      case 'all':
+      default:
+        return { start: new Date(0), end: new Date() };
+    }
+  }
+  
+  exports.getStatistics = async (dateRange) => {
+    const pipeline = [
+        {
+            $match: {
+                createdAt: { $gte: dateRange.start, $lte: dateRange.end }
+            }
+        },
+        {
+            $group: {
+                _id: null,
+                count: { $sum: 1 }
+            }
+        }
+    ];
+
+    const speakingCount = await SpeakingAnalysis.aggregate(pipeline);
+    const writingCount = await WritingConversationAnalysis.aggregate(pipeline);
+    const readingCount = await ReadingAnalysis.aggregate(pipeline);
+    const listeningCount = await ListeningAnalysis.aggregate(pipeline);
+
+    return {
+        speaking: speakingCount.length > 0 ? speakingCount[0].count : 0,
+        writing: writingCount.length > 0 ? writingCount[0].count : 0,
+        reading: readingCount.length > 0 ? readingCount[0].count : 0,
+        listening: listeningCount.length > 0 ? listeningCount[0].count : 0,
+    };
+};
+
+exports.handleGetStatistics = async (req, res) => {
+    try {
+        const dateRange = getDateRange(req.query.timeframe);
+        const stats = await exports.getStatistics(dateRange);
+        res.json(stats);
+    } catch (error) {
+        console.error('Failed to fetch statistics:', error);
+        res.status(500).json({ error: error.message });
+    }
+};
